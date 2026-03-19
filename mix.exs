a57dcd856930140a46b2b0d692f16d6cf88c6bf2
ribanez7 <ruben.ibc@yandex.com>
2026-03-19 09:34:09 +0100

    upgrade latest versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ProtocolFallbackBug.MixProject do
     [
       app: :protocol_fallback_bug,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: []
     ]
